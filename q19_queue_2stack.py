b3ed11c2b2e8389bec26971d803fe1d707342da2
Peter Phelan <peter.phelan@actian.com>
2017-03-21 15:15:08 -0700

    Used 2 stacks instead of 2 lists

--- q19_queue_2stack.py
+++ q19_queue_2stack.py
@@ -1,30 +1,55 @@
+class Stack:
+
+    # initialize an empty list
+    def __init__(self):
+        self.items = []
+
+
+    # push a new item to the last index
+    def push(self, item):
+        self.items.append(item)
+
+
+    def pop(self):
+        if len(self.items) == 0:
+            return None
+        else:
+            return self.items.pop()
+
+
+    def peek(self):
+        if len(self.items) == 0:
+            return None
+        else:
+            return self.items[-1]
+
+
 class Queue:
 
     def __init__(self):
-        self.in_stack  = []
-        self.out_stack = []
-
+        self.in_stack  = Stack()
+        self.out_stack = Stack()
 
 
     def enqueue(self,item):
-        self.in_stack.append(item)
+        self.in_stack.push(item)
 
 
     def dequeue(self):
 
-        if len(self.out_stack) == 0:
-            while len(self.in_stack) > 0:
-                self.out_stack.append(self.in_stack.pop())
+        if len(self.out_stack.items) == 0:
+            while len(self.in_stack.items) != 0:
+                self.out_stack.push(self.in_stack.pop())
 
-            if len(self.out_stack) == 0:
+            if len(self.out_stack.items) == 0:
                 raise IndexError("Can't dequeue from empty queue!")
 
         return self.out_stack.pop()
 
 
     def printStacks(self):
-        print(self.in_stack)
-        print(self.out_stack)
+        print(self.in_stack.items)
+        print(self.out_stack.items)
 
 
 
